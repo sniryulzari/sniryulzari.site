6f8a5c14f73c1c575aca49837ef6e9c168b77930
sniryulzari <sniryulzari@gmail.com>
2023-06-05 10:42:53 +0300

    porfolio website

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require("http");
 
 // server your css as static
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
 http.createServer(function (req, res) {
  res.write("On the way to being a full stack engineer!");
- console.log(__filename);
+//  console.log(__dirname);
+//  res.sendFile('portfolio.html', {root: __dirname})
 //  res.sendFile(__dirname + "/portfolio.html");
  res.end();
 }).listen(3000);
